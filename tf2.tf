b5a3c65bfc8416a579fe252ea29ecabee667678f
Julien <julien@ynov.fr>
2023-07-04 16:43:09 +0200

    2023/07/04 14h06

--- tf2.tf
+++ tf2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-06d9ac15ee83413ec"  # Remplacez "ami-12345678" par l'ID de l'AMI Debian que vous souhaitez utiliser
+  ami           = "ami-088c0417fba33e664"  # Remplacez "ami-12345678" par l'ID de l'AMI Debian que vous souhaitez utiliser
   instance_type = "t2.micro"
 
   tags = {
